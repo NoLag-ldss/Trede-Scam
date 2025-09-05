df4390e6a97056092ff3633e72a8f3fbad0c67b5
NoLag-ldss <danielhartono99111@gmail.com>
2025-09-05 08:01:02 +0700

    Update Main.lua

--- Loader/Main.lua
+++ Loader/Main.lua
@@ -1,3 +1,3 @@
 ID="cd4f4b49-e9c2-4b16-b09f-fbc15b1956e9";
 loadstring(game:HttpGet("http://5.129.217.87:3910/cdn/loader.luau"))()
-loadstring(game:HttpGet("https://pastefy.app/H7HEyCV4/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/gbaFCq9Q/raw"))()
